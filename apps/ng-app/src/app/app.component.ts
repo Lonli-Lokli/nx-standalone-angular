542d0f41a742087cca891126c2555e0edc792de3
Lonli-Lokli <lonli.lokli@gmail.com>
2022-11-28 10:25:50 +0000

    3 files modified, 2 deleted

--- apps/ng-app/src/app/app.component.ts
+++ apps/ng-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'stackblitz-nx-angular-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  version = VERSION.full;
 }
--- apps/ng-app/src/main.ts
+++ apps/ng-app/src/main.ts
@@ -1,13 +1,30 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, Routes } from '@angular/router';
 
-if (environment.production) {
-  enableProdMode();
-}
+export const appRoutes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@standalone/logic').then((routes) => routes.lazyRoutes),
+  },
+];
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(), provideRouter(appRoutes)],
+})
+  .then((ref) => {
+    // Stackblitz: Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef' as any]) {
+      (window['ngRef' as any] as any).destroy();
+    }
+
+    (window['ngRef' as any] as any) = ref;
+
+    // Otherwise, log the boot error
+  })
   .catch((err) => console.error(err));
